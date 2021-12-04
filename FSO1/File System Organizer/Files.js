1554d1ded0751eed978a8981ca3e8e7f319284a9
Mukesh Dani <mukeshdani00@gmail.com>
2021-12-04 20:46:11 +0530

    created a function

--- FSO1/File System Organizer/Files.js
+++ FSO1/File System Organizer/Files.js
@@ -1,3 +1,44 @@
 
+/* 
+let input = process.argv[2]
+console.log ( input );
+ */
+
+let inputArr = process.argv.slice(0)
+//console.log ( inputArr)
+
+
+let cammand = inputArr[2]
+
+//console.log (cammand) 
+
+
+switch(cammand){
+
+
+    case 'tree': treeFn()
+    break ; 
+    case 'organizse': organizeFn()
+    break;
+    case 'Help': HelpFn()
+    break;
+    default:
+        console.log ("WRONG CAMMAND ")
+        break;
+}
+
+
+function organizeFn(){
+    console.log("ORGANIZE FUNCTION IS IMPLEMENTED ")
+}
+
+function treeFn(){
+    console.log("TREE FUNCTION IS IMPLEMENTED ")
+}
+
+function HelpFn(){
+    console.log("HELP FUNCTION IS IMPLEMENTED ")
+}
+
 
 
